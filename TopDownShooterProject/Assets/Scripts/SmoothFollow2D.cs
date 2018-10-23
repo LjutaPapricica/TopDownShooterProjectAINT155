216ff45f56900b826f6eced2004aa8d5183e52de
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2018-10-23 17:41:30 +0100

    Player movement & rotation and camera follow
    The player can move around with 'wasd' controls and rotates to face the position of the mouse pointer. The camera smoothly follows the player as he moves.

---
+++ TopDownShooterProject/Assets/Scripts/SmoothFollow2D.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmoothFollow2D : MonoBehaviour {
+    [SerializeField] Transform target;
+    [SerializeField] float smoothing = 5.0f;
+
+    private void FixedUpdate()
+    {
+        Vector3 newPos = new Vector3(target.position.x, target.position.y, transform.position.z);
+        transform.position = Vector3.Lerp(transform.position, newPos, (smoothing * 0.001f));
+    }
+}
---
+++ TopDownShooterProject/Assets/Scripts/TopDownCharacterController2D.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownCharacterController2D : MonoBehaviour {
+
+    [SerializeField] float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+
+	// Use this for initialization
+	void Start ()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+	}
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
