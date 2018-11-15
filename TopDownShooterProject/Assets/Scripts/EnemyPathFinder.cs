8a5382d8351f39b7e2aa8dd471d880eea1923ba5
ShemSkillman <43577362+ShemSkillman@users.noreply.github.com>
2018-11-15 13:49:55 +0000

    Enemy tank behaviour improvement
    Enemy tanks now have turrets which turn seperately to face the player. Enemy tanks stop when certain distance from player and start moving again once player is out of firing range.

--- TopDownShooterProject/Assets/Scripts/EnemyPathFinder.cs
+++ TopDownShooterProject/Assets/Scripts/EnemyPathFinder.cs
@@ -7,19 +7,38 @@ public class EnemyPathFinder : MonoBehaviour {
 
     public Transform target;
     private IAstarAI ai;
+    private AILerp aiLerp;
+    private Rigidbody2D rb;
 
     private void Start()
     {
         ai = GetComponent<IAstarAI>();
+        aiLerp =  GetComponent<AILerp>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
+        
+
         if (target != null && ai != null)
         {
             ai.destination = target.position;
             ai.SearchPath();
+
+            float dist = Vector3.Distance(transform.position, target.position);
+
+            if (dist < 3)
+            {
+                aiLerp.canMove = false;
+                rb.velocity = new Vector2(0f, 0f);
+            }
+            else if (dist > 6)
+            {
+                aiLerp.canMove = true;
+            }
         }
+
     }
 
     public void SetTarget(Transform newTarget)
---
+++ TopDownShooterProject/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour {
+
+    public Transform turret;
+    public float maxDistance = 100f;
+    float originOffSet = 0.5f;
+
+    //public static RaycastHit2D Raycast(Vector2 origin, Vector2 direction, float distance = Mathf.Infinity, int layerMask = DefaultRaycastLayers, float minDepth = -Mathf.Infinity, float maxDepth = Mathf.Infinity);
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        RaycastHit2D hit = Physics2D.Raycast(turret.position, turret.up, Mathf.Infinity);
+        Debug.DrawRay(turret.position, turret.up, Color.red);
+
+        if (hit.collider != null)
+        {
+            Debug.Log("I hit" + hit.collider.name);
+        }
+	}
+}
